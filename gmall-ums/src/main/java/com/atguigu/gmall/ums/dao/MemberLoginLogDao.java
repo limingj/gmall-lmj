285b5aa421ade9a564e97772c63c0122fe2e979e
李明静 <1816025354@qq.com>
2020-01-02 10:13:36 +0800

    项目初始化

---
+++ gmall-ums/src/main/java/com/atguigu/gmall/ums/dao/MemberLoginLogDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.ums.dao;
+
+import com.atguigu.gmall.ums.entity.MemberLoginLogEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 会员登录记录
+ * 
+ * @author lixianfeng
+ * @email lxf@atguigu.com
+ * @date 2020-01-01 18:46:22
+ */
+@Mapper
+public interface MemberLoginLogDao extends BaseMapper<MemberLoginLogEntity> {
+	
+}
